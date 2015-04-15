e52aa6afd1f946b9abba68a28bd4f8025f7aec8c
Andbondar <andbondar@yandex.ru>
2015-04-15 18:14:38 +0300

    stable test for mailru send-check leter

--- src/test/java/HomeWork02/MailRuTests.java
+++ src/test/java/HomeWork02/MailRuTests.java
@@ -43,9 +43,8 @@ public class MailRuTests {
         } catch (IOException e){
             e.printStackTrace();
         }
-        //temporary fix to combine all the text into solid string without breaks
+        //combine all the text into solid string
         textMessage = textMessage.replace("\n", "<br>").replace("\r", "<br>").replace("<br><br>", "<br>");
-        //textMessage = textMessage.replace("<br><br>", "<br>");
 
         //login
         driver.findElement(By.xpath(".//*[@id='mailbox__login']")).clear();
@@ -57,6 +56,7 @@ public class MailRuTests {
         //check that user is logged in
         String loggedEmail = driver.findElement(By.xpath(".//*[@id='PH_user-email']")).getText();
         assertEquals("Wrong user is logged in", loggedEmail, login + "@mail.ru");
+        int initialLetterCount = Integer.parseInt(driver.findElement(By.xpath(".//*[@class=\"b-nav__item__count\"]")).getText());
         //"write letter" button
         driver.findElement(By.xpath(".//*[@id='b-toolbar__left']//a")).click();
         //check that page is changed and new one is a page for letter writing
@@ -65,10 +65,27 @@ public class MailRuTests {
         driver.findElement(By.xpath(".//textarea[@data-original-name=\"To\"]")).click();
         driver.findElement(By.xpath(".//textarea[@data-original-name=\"To\"]")).sendKeys(login + "@mail.ru" + Keys.RETURN);
 
-        driver.findElement(By.xpath(".//input[@name=\"Subject\"]")).sendKeys("Test letter");
+        String subject = "Test letter";
+        driver.findElement(By.xpath(".//input[@name=\"Subject\"]")).sendKeys(subject);
         //text
         if (driver instanceof JavascriptExecutor) {
             ((JavascriptExecutor) driver).executeScript("window.parent.tinyMCE.activeEditor.setContent('" + textMessage + "');");
         }
+        //send letter
+        driver.findElement(By.xpath(".//*[@data-name=\"send\"]")).click();
+        driver.findElement(By.xpath(".//a[@class=\"b-nav__link\" and @href=\"/messages/inbox/\"]")).click();
+        //check that correct page is opened (inbox)
+        assertTrue("Page is not changed", driver.getCurrentUrl().matches(".*inbox.*"));
+        int updatedLetterCount = Integer.parseInt(driver.findElement(By.xpath(".//*[@class=\"b-nav__item__count\"]")).getText());
+        //check that new email is received by current count of unread emails
+        assertTrue("No new letters", updatedLetterCount > initialLetterCount);
+        //Open this letter
+        driver.findElement(By.xpath("(.//*[@class=\"b-datalist__body\"]//a[@data-subject='" + subject + "'])[1]")).click();//we search latest letter with this theme
+
+        //check that this letter is sent today - update for regex with russian characters needed
+        //boolean flag = driver.findElement(By.xpath(".//*[@class=\"b-letter__head__date\"]")).getText().matches(".*Сегодня.*");
+        //assertTrue("Letter was not sent today", flag);
+
+        //check for text in received letter can be added
     }
 }
